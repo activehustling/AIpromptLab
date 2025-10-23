1b44bf72ecf1867d247907b7dafb84bb6c01b27c
activehustling <favourinnocentjohn0@gmail.com>
2025-10-23 09:29:03 +0100

    Add files via upload
    Signed-off-by: activehustling <favourinnocentjohn0@gmail.com>

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10080731
+    "zoneId": 10082185
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
